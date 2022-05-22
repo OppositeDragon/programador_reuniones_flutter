f6c9abd2e365b0d2e69eadaccadf7e055f71b8c9
leninrkb <rkb.lenin@gmail.com>
2022-05-22 16:55:10 -0500

    creacion pagina loginview

--- lib/views/principal.dart
+++ lib/views/principal.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:programador_reuniones_flutter/views/login_view.dart';
 
 class Principal extends StatelessWidget {
-  const Principal({super.key}) ;
+  const Principal({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Programador de reuniones'),
+        child: Column(
+          children: [
+            Text('Programador de reuniones'),
+            ElevatedButton(
+                onPressed: () {
+                  //Navigator.of(context).push(LoginView());
+                },
+                child: Text("login pro max 21"))
+          ],
+        ),
       ),
     );
   }
