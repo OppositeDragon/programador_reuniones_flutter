b244763f96b02375b2bcd81d332abd556ad2244d
OppositeDragon <stee-ven_94@outlook.com>
2022-05-23 15:22:33 -0500

    Implementacion del tema

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:programador_reuniones_flutter/theme/theme_controller.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:programador_reuniones_flutter/views/principal.dart';
 
 void main() async {
-	WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key}) ;
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Programador de reuniones',
-      theme: ThemeData(
-        colorSchemeSeed: Colors.deepPurple,
-      ),
+      theme: ref.watch(themeProvider).themeData,
       home: const Principal(),
     );
   }
--- lib/views/principal.dart
+++ lib/views/principal.dart
@@ -5,8 +5,9 @@ class Principal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return  Scaffold(
+			appBar: AppBar(actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.settings))]),
+      body:const Center(
         child: Text('Programador de reuniones'),
       ),
     );
