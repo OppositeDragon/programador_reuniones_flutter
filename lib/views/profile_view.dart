57b549f5e6950b038fe525cca7da4b58bdb96308
leninrkb <rkb.lenin@gmail.com>
2022-05-23 17:20:22 -0500

    Update profile_view.dart

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -14,77 +14,80 @@ class _ProfileViewState extends State<ProfileView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: SingleChildScrollView(
-        child: AnimatedContainer(
-          padding: const EdgeInsets.all(24),
-          //	color: Colors.yellow,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            //color: Colors.grey.shade200
-          ),
-          constraints: const BoxConstraints(maxWidth: 500),
-          duration: const Duration(milliseconds: 2000),
-          child: Center(
-            child: Form(
-                child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(50.0),
-                  child: Image.network(
-                    "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
-                    height: 150,
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Usuario",
-                    border: OutlineInputBorder(),
-                    hintText: "NobMaster69",
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: "Telfono",
-                    border: OutlineInputBorder(),
-                    hintText: "0927592640",
-                  ),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: "Nombre",
-                      hintText: "Julian",
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: "Apellido",
-                      hintText: "Julian",
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 20),
-                TextFormField(
-                  decoration: const InputDecoration(
-                      labelText: "Contrasenia",
-                      hintText: "***********",
-                      border: OutlineInputBorder()),
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () => GoRouter.of(context).go('/'),
-                  child: const Text("Actualizar mis datos"),
-                )
-              ],
-            )),
-          ),
+        appBar: AppBar(
+          title: const Text("Perfil"),
         ),
-      ),
-    ));
+        body: Center(
+          child: SingleChildScrollView(
+            child: AnimatedContainer(
+              padding: const EdgeInsets.all(24),
+              //	color: Colors.yellow,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                //color: Colors.grey.shade200
+              ),
+              constraints: const BoxConstraints(maxWidth: 500),
+              duration: const Duration(milliseconds: 2000),
+              child: Center(
+                child: Form(
+                    child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50.0),
+                      child: Image.network(
+                        "https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg",
+                        height: 150,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Usuario",
+                        border: OutlineInputBorder(),
+                        hintText: "NobMaster69",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        labelText: "Telfono",
+                        border: OutlineInputBorder(),
+                        hintText: "0927592640",
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: "Nombre",
+                          hintText: "Julian",
+                          border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: "Apellido",
+                          hintText: "Julian",
+                          border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: "Contrasenia",
+                          hintText: "***********",
+                          border: OutlineInputBorder()),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => GoRouter.of(context).go('/'),
+                      child: const Text("Actualizar mis datos"),
+                    )
+                  ],
+                )),
+              ),
+            ),
+          ),
+        ));
   }
 }
