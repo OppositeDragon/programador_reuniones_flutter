33e27b61125b65f191941a4ef528d1486b782ff3
OppositeDragon <stee-ven_94@outlook.com>
2022-06-20 16:02:39 -0500

    pass theme instead of single color, Horario semanal se imprime correctamente.

--- lib/painters/horario_painter.dart
+++ lib/painters/horario_painter.dart
@@ -6,22 +6,25 @@ import 'package:programador_reuniones_flutter/models/enums.dart';
 import 'package:programador_reuniones_flutter/models/horario_personal_model.dart';
 
 class HorarioPainter extends CustomPainter {
-  final Color primaryColor;
   final ui.Brightness brightness;
-  final SemanaHorarioPersonalModel horarioSemanal;
+  final ThemeData theme;
   final Map<String, dynamic> update;
-  HorarioPainter({required this.primaryColor, required this.brightness, required this.update, required this.horarioSemanal});
+  final SemanaHorarioPersonalModel horarioSemanal;
+  HorarioPainter({
+    required this.brightness,
+    required this.theme,
+    required this.update,
+    required this.horarioSemanal,
+  });
 
   @override
   void paint(Canvas canvas, Size size) {
-    //
-    print('chuncksoftime: ${horarioSemanal.D.getChuncksOfTime()}');
 //calculate offsets
     const double bottomMargin = 10;
     const double leftMargin = 45;
     final hOffset = (size.width - leftMargin) / 7;
     final vOffset = (size.height - bottomMargin) / 96;
-    print('vOffset: $vOffset');
+    final textColor = theme.textTheme.bodyText1!.color!;
     final hourLineColor = brightness == ui.Brightness.dark ? const Color.fromRGBO(235, 235, 235, 0.8) : const Color.fromRGBO(45, 45, 45, 0.8);
     final midHourLineColor = brightness == ui.Brightness.dark ? const Color.fromRGBO(160, 160, 160, 0.8) : const Color.fromRGBO(90, 90, 90, 0.8);
 
@@ -41,6 +44,14 @@ class HorarioPainter extends CustomPainter {
       ..color = midHourLineColor
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 1;
+    final timeBrush = Paint()
+      ..color = theme.colorScheme.secondary
+      ..style = ui.PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 2;
+    final timeBrush2 = Paint()..color = theme.colorScheme.secondary.withOpacity(0.3);
+    const radiusRRect = Radius.circular(3);
+
 //
     final updateBrush = Paint()
       ..color = Colors.indigo.shade900.withOpacity(0.5)
@@ -53,8 +64,8 @@ class HorarioPainter extends CustomPainter {
     //
 //text
     final paragraphStyle = ui.ParagraphStyle(textDirection: TextDirection.ltr, textAlign: TextAlign.right);
-    final textStyleBig = ui.TextStyle(color: primaryColor, fontSize: 11, fontWeight: FontWeight.w600);
-    final textStyleSmall = ui.TextStyle(color: primaryColor, fontSize: 9);
+    final textStyleBig = ui.TextStyle(color: textColor, fontSize: 11, fontWeight: FontWeight.w600);
+    final textStyleSmall = ui.TextStyle(color: textColor, fontSize: 9);
 //draw the timetext
     for (var i = 0; i < Contstants.horasDia.length; i++) {
       final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle)
@@ -70,13 +81,12 @@ class HorarioPainter extends CustomPainter {
     canvas.drawLine(const Offset(44, 0.5), Offset(size.width, 0.5), hourBrush);
     for (int i = 1; i <= 96; i++) {
       final double dy = i * vOffset + 1;
-
       if (i % 4 == 0) {
         canvas.drawLine(Offset(40, dy), Offset(size.width, dy), hourBrush);
       } else if (i % 2 == 0) {
         drawDashedLine(canvas, size, midHourBrush, leftMargin, dy, 3, 3);
       } else {
-        drawDashedLine(canvas, size, quarterHourBrush, leftMargin, dy, 3, 1);
+        if (size.height > 700) drawDashedLine(canvas, size, quarterHourBrush, leftMargin, dy, 3, 1);
       }
     }
 
@@ -84,6 +94,40 @@ class HorarioPainter extends CustomPainter {
     for (var i = 7; i >= 0; i--) {
       final double dx = (size.width - 0.5) - i * hOffset;
       canvas.drawLine(Offset(dx, 0), Offset(dx, size.height - bottomMargin), verticalBrush);
+    }
+
+    //
+
+    final List<DiaHorarioPersonal> horarioDias = [
+      horarioSemanal.D,
+      horarioSemanal.L,
+      horarioSemanal.M,
+      horarioSemanal.X,
+      horarioSemanal.J,
+      horarioSemanal.V,
+      horarioSemanal.S
+    ];
+    double dx = 45;
+    for (int i = 0; i < horarioDias.length; i++) {
+      for (var element in horarioDias[i].getChuncksOfTime().entries) {
+        final double dy1 = TimeSlot.values.firstWhere((timesSlot) => timesSlot.start == element.key).i * vOffset;
+        final double dy2 = TimeSlot.values.firstWhere((timesSlot) => timesSlot.start == element.value).i * vOffset + vOffset;
+        final Rect rect = Rect.fromPoints(Offset(dx + 1, dy1 + 2.5), Offset(dx + hOffset - 2, dy2 - 0.5));
+        print('dy1: $dy1, dy2: $dy2');
+        canvas.drawRRect(
+            RRect.fromRectAndRadius(
+              rect,
+              radiusRRect,
+            ),
+            timeBrush2);
+        canvas.drawRRect(
+            RRect.fromRectAndRadius(
+              rect,
+              radiusRRect,
+            ),
+            timeBrush);
+      }
+      dx += hOffset;
     }
   }
 
@@ -93,7 +137,7 @@ class HorarioPainter extends CustomPainter {
       print('update ${update == oldDelegate.update}');
       print('newupdate ${update['onLongPressStart']} ${update['onLongPressEnd']}');
       print('oldupdate ${oldDelegate.update['onLongPressStart']} ${oldDelegate.update['onLongPressEnd']}');
-      print('shouldRepaint ${oldDelegate is HorarioPainter && (primaryColor != oldDelegate.primaryColor || update != oldDelegate.update)}');
+      print('shouldRepaint ${oldDelegate is HorarioPainter && (theme != oldDelegate.theme || update != oldDelegate.update)}');
     }
     return true; //oldDelegate is HorarioPainter && (primaryColor != oldDelegate.primaryColor || update != oldDelegate.update);
   }
