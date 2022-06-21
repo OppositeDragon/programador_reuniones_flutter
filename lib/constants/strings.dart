4d903f295c85e5f88bd02ed9d37cb069bdc06532
Jean152j <74079643+Jean152j@users.noreply.github.com>
2022-06-21 16:26:55 -0500

    Creación de clase strings para almacenar las constantes de las cadenas de texto

---
+++ lib/constants/strings.dart
@@ -0,0 +1,7 @@
+class Strings {
+  Strings._();
+
+  static const String buttonCerrarSesion = "Cerrar sesión";
+  static const String buttonTema = "Tema";
+  static const String buttonPerfil = "Perfil";
+}
