f472670746dfcc4e3b35568b1087fd2c35482d67
Jean152j <74079643+Jean152j@users.noreply.github.com>
2022-06-21 17:49:23 -0500

    Creación de nuevas constantes para ser utilizadas en las diferentes clases

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -4,4 +4,16 @@ class Strings {
   static const String buttonCerrarSesion = "Cerrar sesión";
   static const String buttonTema = "Tema";
   static const String buttonPerfil = "Perfil";
+  static const String labelGrupo = "Crear grupo";
+  static const String labelEditarGrupo = "Editar grupo";
+  static const String msgAdmin = "Solo el administrador del grupo puede editarlo";
+  static const String buttonReturnInicio = "Volver a inicio";
+  static const String buttonDetailGrupo = "Detalle del grupo";
+  static const String labelNameGrupo = "Nombre de grupo";
+  static const String msgNameGrupo = "Ingrese el nombre del grupo";
+  static const String labelDescGrupo = "Descripción de grupo";
+  static const String msgDescGrupo = "Ingrese descripción del grupo (mínimo 10 caracteres)";
+  static const String labelBuscar = "Buscar integrantes: ";
+  static const String labelProcesando = "Procesando";
+  static const String labelCancelar = "Cancelar";
 }
