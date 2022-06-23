a1eb5fe9cbcb8d4c6d2b3b120ec6d1e074a56658
OppositeDragon <stee-ven_94@outlook.com>
2022-06-22 19:06:28 -0500

    Optimize horario_painter

--- lib/painters/horario_painter.dart
+++ lib/painters/horario_painter.dart
@@ -18,7 +18,6 @@ class HorarioPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
     //calculate offsets
-
     final hOffset = (size.width - Contstants.leftMargin) / 7;
     final vOffset = (size.height - Contstants.bottomMargin) / 96;
     // colors to be used
@@ -48,39 +47,35 @@ class HorarioPainter extends CustomPainter {
       ..style = ui.PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
       ..strokeWidth = 2;
+    final timeBrush2 = Paint()..color = theme.colorScheme.secondary.withOpacity(0.3);
     final updateBrushFill = Paint()..color = theme.colorScheme.primary.withOpacity(0.65);
     final updateBrushStroke = Paint()
       ..color = theme.colorScheme.primary.withOpacity(0.85)
       ..style = ui.PaintingStyle.stroke
       ..strokeWidth = 3;
-    final timeBrush2 = Paint()..color = theme.colorScheme.secondary.withOpacity(0.3);
 
-//
-
-   
-
-    // // draw drag update
-    if (update['onLongPressStart'] != null && update['onLongPressEnd'] != null) { print('${update['onLongPressStart']!} ${update['onLongPressEnd']!}');
+    // draw drag update
+    if (update['onLongPressStart'] != null && update['onLongPressEnd'] != null) {
+      print('${update['onLongPressStart']!} ${update['onLongPressEnd']!}');
       final Rect rect = Rect.fromPoints(update['onLongPressStart']!, update['onLongPressEnd']!);
       canvas.drawRRect(RRect.fromRectAndRadius(rect, Contstants.radiusRRect), updateBrushFill);
       canvas.drawRRect(RRect.fromRectAndRadius(rect, Contstants.radiusRRect), updateBrushStroke);
     }
-    //
     //text configuration
     final paragraphStyle = ui.ParagraphStyle(textDirection: TextDirection.ltr, textAlign: TextAlign.right);
     final textStyleBig = ui.TextStyle(color: textColor, fontSize: 11, fontWeight: FontWeight.w600);
     final textStyleSmall = ui.TextStyle(color: textColor, fontSize: 9);
+    const widhtConstraints43 = ui.ParagraphConstraints(width: 43);
+    const widhtConstraints40 = ui.ParagraphConstraints(width: 40);
     //draw the timetext
     for (var i = 0; i < Contstants.horasDia.length; i++) {
       final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle)
         ..pushStyle((i % 2 == 0) ? textStyleSmall : textStyleBig)
         ..addText(Contstants.horasDia[i]);
-      final paragraph = paragraphBuilder.build()
-        ..layout((i % 2 == 0) ? const ui.ParagraphConstraints(width: 43) : const ui.ParagraphConstraints(width: 40));
+      final paragraph = paragraphBuilder.build()..layout((i % 2 == 0) ? widhtConstraints43 : widhtConstraints40);
       final double dy = ((i + 1) * vOffset * 2) - 3;
       canvas.drawParagraph(paragraph, (i % 2 == 0) ? Offset(0, dy) : Offset(0, dy - 2));
     }
-
     //draws the horizontal lines
     canvas.drawLine(const Offset(44, 0.5), Offset(size.width, 0.5), hourBrush);
     for (int i = 1; i <= 96; i++) {
@@ -93,24 +88,13 @@ class HorarioPainter extends CustomPainter {
         if (size.height > 700) drawDashedLine(canvas, size, quarterHourBrush, Contstants.leftMargin, dy, 3, 1);
       }
     }
-
     //draws vertical lines
     for (var i = 7; i >= 0; i--) {
       final double dx = (size.width - 0.5) - i * hOffset;
       canvas.drawLine(Offset(dx, 0), Offset(dx, size.height - Contstants.bottomMargin), verticalBrush);
     }
-
     //
-
-    final List<DiaHorarioPersonal> horarioDias = [
-      horarioSemanal.D,
-      horarioSemanal.L,
-      horarioSemanal.M,
-      horarioSemanal.X,
-      horarioSemanal.J,
-      horarioSemanal.V,
-      horarioSemanal.S
-    ];
+    final List<DiaHorarioPersonal> horarioDias = horarioSemanal.toListOfDays();
     double dx = 45;
     for (int i = 0; i < horarioDias.length; i++) {
       for (var element in horarioDias[i].getChuncksOfTime().entries) {
@@ -126,13 +110,8 @@ class HorarioPainter extends CustomPainter {
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    if (oldDelegate is HorarioPainter) {
-      print('update ${update == oldDelegate.update}');
-      print('newupdate ${update['onLongPressStart']} ${update['onLongPressEnd']}');
-      print('oldupdate ${oldDelegate.update['onLongPressStart']} ${oldDelegate.update['onLongPressEnd']}');
-      print('shouldRepaint ${oldDelegate is HorarioPainter && (theme != oldDelegate.theme || update != oldDelegate.update)}');
-    }
-    return true; //oldDelegate is HorarioPainter && (primaryColor != oldDelegate.primaryColor || update != oldDelegate.update);
+    return oldDelegate is HorarioPainter &&
+        (theme != oldDelegate.theme || update != oldDelegate.update || horarioSemanal == oldDelegate.horarioSemanal);
   }
 
   void drawDashedLine(ui.Canvas canvas, ui.Size size, ui.Paint brush, double leftMargin, double dy, int space, int dash) {
