00baa01eefd5428fc643f02108872a9dc564c593
OppositeDragon <stee-ven_94@outlook.com>
2022-07-19 20:01:03 -0500

    documentacion del controlador de login

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -6,28 +6,45 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final loginProvider = ChangeNotifierProvider<LoginController>((ref) => LoginController());
 
+/// Clase que controla el login de usuarios
 class LoginController with ChangeNotifier {
+  /// Indica si la aplicacion esta intentado establecer una conexion con Firebase y autenticar el usuario
   bool _isLoading = false;
+
+  /// Indica si se debe mostror la clave del usuario
   bool _showPassword = false;
+
+  /// Indica que usuario esta autenticado, [null] si no hay usuario autenticado
   User? _user;
+
+  /// Obtiene el usuario autenticado
   User? get user => _user;
+
+  /// Obtiene el estado de la conexion con Firebase, [returns] [bool]
   bool get isLoading => _isLoading;
+
+  /// Obtiene el estado de visibilidad de la clave del usuario
   bool get showPassword => _showPassword;
+
+  /// Establece el usuario atenticado, guardandolo en la variable [_user]
   setUser() async {
     _user = await FirebaseAuth.instance.authStateChanges().first;
     notifyListeners();
   }
 
+  ///Establece el estado de la conexion con Firebase, guardandolo en la variable [_isLoading]
   set isLoading(bool isLoading) {
     _isLoading = isLoading;
     notifyListeners();
   }
 
+  /// Establece el estado de visibilidad de la clave del usuario, guardandolo en la variable [_showPassword]
   set showPassword(bool showPassword) {
     _showPassword = showPassword;
     notifyListeners();
   }
 
+  /// Permite crear con [email] y [password], ademas [returns] [UserCredential] envuelto en [Future].
   Future<UserCredential> createEmailPassword(String email, String password) async {
     late UserCredential authResult;
     try {
@@ -48,6 +65,8 @@ class LoginController with ChangeNotifier {
     return authResult;
   }
 
+  /// Permite iniciar sesion con [email] y [password], ademas [returns] [UserCredential] envuelto en [Future].
+  /// Throws [FirebaseAuthException] si existe algun problema con el inicio de sesion.
   Future<UserCredential?> loginEmailPassword(String email, String password) async {
     try {
       final authResult = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: password);
@@ -64,6 +83,8 @@ class LoginController with ChangeNotifier {
     return null;
   }
 
+  /// Permite iniciar sesion con [GoogleAuthProvider],
+  /// llama [signInWithPopup] si la plataforma es [kIsWeb] o [GoogleSignIn] a en plataformas moviles.
   Future<void> loginGoogle() async {
     if (kIsWeb) {
       GoogleAuthProvider googleProvider = GoogleAuthProvider();
@@ -89,6 +110,7 @@ class LoginController with ChangeNotifier {
     notifyListeners();
   }
 
+  /// Permite iniciar sesion con [FacebookAuthProvider],
   Future<void> loginFacebook() async {
     // Trigger the sign-in flow
     final LoginResult loginResult = await FacebookAuth.instance.login();
@@ -99,6 +121,7 @@ class LoginController with ChangeNotifier {
     notifyListeners();
   }
 
+  /// Permite cerrar sesion llamando a [FirebaseAuth.instance.signOut]
   Future<void> logout() async {
     await FirebaseAuth.instance.signOut();
     notifyListeners();
