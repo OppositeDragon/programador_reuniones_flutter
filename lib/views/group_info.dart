682f3d7f33d6ce7508d92f1164f747282f96332b
leninrkb <rkb.lenin@gmail.com>
2022-07-24 14:21:43 -0500

    Create group_info.dart

---
+++ lib/views/group_info.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GroupInfo extends StatefulWidget {
+  const GroupInfo({Key? key}) : super(key: key);
+
+  @override
+  State<GroupInfo> createState() => _GroupInfoState();
+}
+
+class _GroupInfoState extends State<GroupInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
